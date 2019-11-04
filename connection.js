c3dd5ce89888bdd832e94e195c7547e46b0b2c63
An's Panjaitan <andreapanjaitan@gmail.com>
2019-11-04 15:37:20 +0700

    Config DB GRATISSS

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "express"
+  host: "db4free.net",
+  user: "ecmlala",
+  password: "andrearif25",
+  database: "ecmlala"
 });
 
 connection.connect(function (err){
